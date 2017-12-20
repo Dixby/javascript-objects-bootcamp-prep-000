d6c742a5c9fda5820cf188ce99f9fbaf888294ac
Andrew Walker <hovercar00@gmail.com>
2017-12-20 03:00:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,10 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
+
