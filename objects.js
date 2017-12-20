4ccb6bd2a178365e6b03bdc0db21840d51ddd13a
Andrew Walker <hovercar00@gmail.com>
2017-12-20 02:57:58 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  'Chet Baker': 'Drop the Game',
+  'Seafrets': 'Oceans'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = 'songTitle';
+  
+}
